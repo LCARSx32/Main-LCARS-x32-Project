82b9e5d07bd6606bd7ffeb4487fb8f74d2860b61
Surak256 <surak1024@gmail.com>
2013-06-28 20:59:12 -0400

    Created x32Interop class.

---
+++ LCARSbuttons/LCARSbuttons/Classes/x32Interop.vb
@@ -0,0 +1,17 @@
+﻿Public Class x32Interop
+#Region " Events "
+    Public Event WorkingAreaChanged(ByVal NewArea As System.Drawing.Rectangle)
+    Public Event AlertInitiated(ByVal AlertID As Integer)
+    Public Event AlertEnded As EventHandler
+    Public Event BeepingChanged(ByVal Beeping As Boolean)
+    Public Event ColorsChanged()
+#End Region
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        MyBase.WndProc(m)
+    End Sub
+
+    Public Sub Init()
+        'Initialize reference to x32 and start receiving events
+    End Sub
+End Class
