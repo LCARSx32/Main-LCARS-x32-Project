b1bd22c862c09feceb731defb1a5e57e7d6e9b89
Surak256 <surak1024@gmail.com>
2013-08-17 03:50:26 -0400

    Added CTypes for LCComplex and LCHalfPill buttons.

--- LCARSbuttons/LCARSbuttons/Lightweight Controls/LCHalfPillButton.vb
+++ LCARSbuttons/LCARSbuttons/Lightweight Controls/LCHalfPillButton.vb
@@ -65,5 +65,51 @@ Namespace LightweightControls
                 End If
             End Set
         End Property
+
+#Region " Conversion "
+        Public Overloads Shared Widening Operator CType(ByVal o As LCHalfPillButton) As LCARS.Controls.HalfPillButton
+            Dim newButton As New LCARS.Controls.HalfPillButton
+            newButton.Text = o.Text
+            newButton.Bounds = o.Bounds
+            newButton.Data = o.Data
+            newButton.Data2 = o.Data2
+            newButton.ColorsAvailable = o.ColorsAvailable
+            newButton.Color = o.Color
+            newButton.AutoEllipsis = o.AutoEllipsis
+            newButton.ButtonTextAlign = o.TextAlign
+            newButton.Clickable = o.Clickable
+            newButton.Flash = o.Flashing
+            newButton.FlashInterval = CInt(o.FlashInterval)
+            newButton.Font = o.Font
+            newButton.holdDraw = o.HoldDraw
+            newButton.CustomAlertColor = o.CustomAlertColor
+            newButton.RedAlert = o.RedAlert
+            newButton._ForceCaps = o.ForceCaps
+            newButton.ButtonStyle = o.PillDirection
+            Return newButton
+        End Operator
+        Public Overloads Shared Narrowing Operator CType(ByVal o As LCARS.Controls.HalfPillButton) As LCHalfPillButton
+            Dim newButton As New LCHalfPillButton
+            newButton.Text = o.Text
+            newButton.Bounds = o.Bounds
+            newButton.Data = o.Data
+            newButton.Data2 = o.Data2
+            newButton.ColorsAvailable = o.ColorsAvailable
+            newButton.Color = o.Color
+            newButton.AutoEllipsis = o.AutoEllipsis
+            newButton.TextAlign = o.ButtonTextAlign
+            newButton.Clickable = o.Clickable
+            newButton.Flashing = o.Flash
+            newButton.FlashInterval = CInt(o.FlashInterval)
+            newButton.Font = o.Font
+            newButton.HoldDraw = o.holdDraw
+            newButton.CustomAlertColor = o.CustomAlertColor
+            newButton.RedAlert = o.RedAlert
+            newButton.ForceCaps = o._ForceCaps
+            newButton.PillDirection = o.ButtonStyle
+            Return newButton
+        End Operator
+#End Region
     End Class
+
 End Namespace
