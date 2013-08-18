40626f21de0109d8e0c8854643f93ee956635435
Surak256 <surak1024@gmail.com>
2013-08-18 01:23:39 -0400

    Added CTypes for LCArrowButton.

--- LCARSbuttons/LCARSbuttons/Lightweight Controls/LCArrowButton.vb
+++ LCARSbuttons/LCARSbuttons/Lightweight Controls/LCArrowButton.vb
@@ -51,5 +51,51 @@ Namespace LightweightControls
                 Redraw()
             End Set
         End Property
+
+#Region " Conversion "
+        Public Overloads Shared Widening Operator CType(ByVal o As LCArrowButton) As LCARS.Controls.ArrowButton
+            Dim newButton As New LCARS.Controls.ArrowButton
+            newButton.Text = o.Text
+            newButton.Bounds = o.Bounds
+            newButton.Data = o.Data
+            newButton.Data2 = o.Data2
+            newButton.ColorsAvailable = o.ColorsAvailable
+            newButton.Color = o.Color
+            newButton.AutoEllipsis = o.AutoEllipsis
+            newButton.ButtonTextAlign = o.TextAlign
+            newButton.Clickable = o.Clickable
+            newButton.Flash = o.Flashing
+            newButton.FlashInterval = CInt(o.FlashInterval)
+            newButton.Font = o.Font
+            newButton.holdDraw = o.HoldDraw
+            newButton.CustomAlertColor = o.CustomAlertColor
+            newButton.RedAlert = o.RedAlert
+            newButton._ForceCaps = o.ForceCaps
+            newButton.ArrowDirection = o.ArrowDirection
+            Return newButton
+        End Operator
+        Public Overloads Shared Narrowing Operator CType(ByVal o As LCARS.Controls.ArrowButton) As LCArrowButton
+            Dim newButton As New LCArrowButton
+            newButton.Text = o.Text
+            newButton.Bounds = o.Bounds
+            newButton.Data = o.Data
+            newButton.Data2 = o.Data2
+            newButton.ColorsAvailable = o.ColorsAvailable
+            newButton.Color = o.Color
+            newButton.AutoEllipsis = o.AutoEllipsis
+            newButton.TextAlign = o.ButtonTextAlign
+            newButton.Clickable = o.Clickable
+            newButton.Flashing = o.Flash
+            newButton.FlashInterval = CInt(o.FlashInterval)
+            newButton.Font = o.Font
+            newButton.HoldDraw = o.holdDraw
+            newButton.CustomAlertColor = o.CustomAlertColor
+            newButton.RedAlert = o.RedAlert
+            newButton.ForceCaps = o._ForceCaps
+            newButton.ArrowDirection = o.ArrowDirection
+            Return newButton
+        End Operator
+#End Region
     End Class
+
 End Namespace
