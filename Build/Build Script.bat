0135c2bc73f9308abc8f493ec25124bea8765518
Surak256 <surak1024@gmail.com>
2013-09-06 10:57:37 -0400

    Added check for compilation failure.

--- Build/Build Script.bat
+++ Build/Build Script.bat
@@ -9,17 +9,22 @@ rmdir /S /Q Out > nul
 mkdir .\Out
 echo Clean complete.
 echo Compiling project...
-devenv ..\LCARSmain\LCARSmain.sln /Build "Release|AnyCPU" > nul && echo Main project compile succeeded.
+devenv ..\LCARSmain\LCARSmain.sln /Build "Release|AnyCPU" > nul || goto CompileError
+echo Main project compile succeeded.
 echo Compressing output...
 zipit.exe .\Out\CurrentVersion.zip -D ..\LCARSmain\LCARSmain\bin\Debug -r+ -j- -E *.* > nul && echo Compression complete.
 echo Moving files...
 xcopy /Y .\Out\CurrentVersion.zip "..\Standard Installer\Standard Installer\Resources\" > nul
 echo Compiling installer...
-devenv "..\Standard Installer\Standard Installer.sln" /Build "Release|AnyCPU" > nul && echo Compile Complete.
+devenv "..\Standard Installer\Standard Installer.sln" /Build "Release|AnyCPU" > nul || goto CompileError
+echo Compile Complete.
 echo Linking files
 ilmerge /lib:"..\Standard Installer\Standard Installer\bin\release" "Standard Installer.exe"  Ionic.zip.reduced.dll Interop.IWshRuntimeLibrary.dll  /target:winexe  /out:.\Out\LCARSx32Installer.exe
 echo Build is complete.
 echo Rename output files to include version number.
+goto End
+:CompileError
+echo Compile failed. Build will terminate.
 goto End
 :ILMergeError
 echo ILMerge was not found. Please download and copy ILMerge.exe to this directory.
