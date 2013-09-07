34d379a593240ccd3a10cf0c7510d7451c71125d
Surak256 <surak1024@gmail.com>
2013-09-07 01:27:48 -0400

    Documented interfaces.

--- LCARSbuttons/LCARSbuttons/Interfaces/IBeeping.vb
+++ LCARSbuttons/LCARSbuttons/Interfaces/IBeeping.vb
@@ -1,3 +1,13 @@
-﻿Public Interface IBeeping
+﻿''' <summary>
+''' Marks a class as having a beeping property
+''' </summary>
+''' <remarks>
+''' This is most often used to allow for a control to match the global beeping setting. Implementation
+''' will vary based on whether there are subcontrols.
+''' </remarks>
+Public Interface IBeeping
+    ''' <summary>
+    ''' Sets the beeping property of the control.
+    ''' </summary>
     Property Beeping() As Boolean
 End Interface
--- LCARSbuttons/LCARSbuttons/Interfaces/IColorable.vb
+++ LCARSbuttons/LCARSbuttons/Interfaces/IColorable.vb
@@ -1,3 +1,12 @@
-﻿Public Interface IColorable
+﻿''' <summary>
+''' Marks a class as using LCARS colors and allows those colors to be accessed.
+''' </summary>
+''' <remarks>
+''' This interface exposes the LCARScolor class, but that class is rarely directly set.
+''' </remarks>
+Public Interface IColorable
+    ''' <summary>
+    ''' Exposes LCARScolor class used by the control for color management
+    ''' </summary>
     Property ColorsAvailable() As LCARS.LCARScolor
 End Interface
