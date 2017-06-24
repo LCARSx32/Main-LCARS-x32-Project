4a4a4ecc9c4588ed1904f0f67b2e443797e97d51
Surak256 <surak1024@gmail.com>
2017-06-24 02:51:22 -0400

    Further refactoring of programList.vb

--- LCARSmain/LCARSmain/Modules/programList.vb
+++ LCARSmain/LCARSmain/Modules/programList.vb
@@ -1,6 +1,7 @@
 Option Strict On
 
 Imports System.Runtime.InteropServices
+Imports System.IO
 
 Public Module programList
 
@@ -71,7 +72,7 @@ Public Module programList
 
     Public ReadOnly Property GetAllPrograms() As DirectoryStartItem
         Get
-            startItems = GetPrograms()
+            startItems = GetStartMenuItems()
             Return startItems
         End Get
     End Property
@@ -87,84 +88,70 @@ Public Module programList
 
 #End Region
 
-
-    Private Function GetPrograms(Optional ByVal path As String = "") As DirectoryStartItem
+    Private Function GetStartMenuItems() As DirectoryStartItem
+        Dim element As DirectoryStartItem = Nothing
         Dim GlobalStartPath As String = ""
         Dim UserStartPath As String = ""
-        Dim mydir As System.IO.DirectoryInfo
-        Dim myFiles() As System.IO.FileSystemInfo
-        Dim myDirs() As System.IO.FileSystemInfo
-        Dim intloop As Integer
+        Select Case System.Environment.OSVersion.Platform
+            Case PlatformID.Win32Windows
+                GlobalStartPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Programs)
+                UserStartPath = ""
+            Case PlatformID.Win32NT
+                Select Case (System.Environment.OSVersion.Version.Major)
+                    Case 3, 4
+                        GlobalStartPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Programs)
+                        UserStartPath = ""
+                    Case Is > 4
+                        Dim myReg As Microsoft.Win32.RegistryKey = Microsoft.Win32.Registry.CurrentUser
+                        myReg = myReg.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\explorer\Shell Folders\", False)
+
+                        UserStartPath = myReg.GetValue("Programs").ToString()
+                        element = GetPrograms(UserStartPath)
+
+                        myReg = Microsoft.Win32.Registry.LocalMachine
+                        myReg = myReg.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\explorer\Shell Folders\", False)
+
+                        GlobalStartPath = myReg.GetValue("Common Programs").ToString()
+                End Select
+        End Select
+        Dim globalItems As DirectoryStartItem = GetPrograms(GlobalStartPath)
+        If element IsNot Nothing Then
+            mergeDirs(globalItems, element)
+        End If
+        element = globalItems
+        Return element
+    End Function
+
+
+    Private Function GetPrograms(ByVal path As String) As DirectoryStartItem
+        Dim mydir As New System.IO.DirectoryInfo(path)
+        Dim myFiles() As System.IO.FileInfo = mydir.GetFiles()
+        Dim myDirs() As System.IO.DirectoryInfo = mydir.GetDirectories()
         Dim curDirectory As DirectoryStartItem
         Dim curFile As FileStartItem
-        Dim element As DirectoryStartItem = Nothing
-        Dim myLinkInfo As New LNKinfo
+        Dim element As New DirectoryStartItem()
+        Dim myLinkInfo As New LNKinfo()
 
-        If path = "" Then
-            Select Case System.Environment.OSVersion.Platform
-                Case PlatformID.Win32Windows
-                    GlobalStartPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Programs)
-                    UserStartPath = ""
-                Case PlatformID.Win32NT
-                    Select Case (System.Environment.OSVersion.Version.Major)
-                        Case 3
-                            GlobalStartPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Programs)
-                            UserStartPath = ""
-                        Case 4
-                            GlobalStartPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Programs)
-                            UserStartPath = ""
-                        Case Is > 4
-                            Dim myReg As Microsoft.Win32.RegistryKey = Microsoft.Win32.Registry.CurrentUser
-                            myReg = myReg.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\explorer\Shell Folders\", False)
+        For intloop As Integer = 0 To myDirs.GetUpperBound(0)
+            curDirectory = GetPrograms(myDirs(intloop).FullName)
+            curDirectory.Name = myDirs(intloop).Name
+            curDirectory.icon = DirectoryIcon
+            element.subItems.Add(curDirectory)
+        Next
 
-                            UserStartPath = myReg.GetValue("Programs").ToString()
-                            element = GetPrograms(UserStartPath)
+        For Each myFile As FileInfo In myFiles
+            If Not (myFile.Name.ToLower = "desktop.ini" Or myFile.Name.ToLower = "thumbs.db") Then
 
-                            myReg = Microsoft.Win32.Registry.LocalMachine
-                            myReg = myReg.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\explorer\Shell Folders\", False)
+                curFile = New FileStartItem()
+                curFile.Name = myFile.Name
 
-                            GlobalStartPath = myReg.GetValue("Common Programs").ToString()
-                    End Select
-            End Select
+                myLinkInfo.Executable = myFile.FullName
+                'curFile.icon = getIcon(myLinkInfo.Executable)
+                curFile.Link = myLinkInfo
 
-
-            Dim globalItems As DirectoryStartItem = GetPrograms(GlobalStartPath)
-            If element IsNot Nothing Then
-                mergeDirs(globalItems, element)
+                element.subItems.Add(curFile)
             End If
-            element = globalItems
-
-
-        Else
-            element = New DirectoryStartItem()
-            mydir = New System.IO.DirectoryInfo(path)
-
-
-            myDirs = mydir.GetDirectories
-
-            For intloop = 0 To myDirs.GetUpperBound(0)
-                curDirectory = GetPrograms(myDirs(intloop).FullName)
-                curDirectory.Name = myDirs(intloop).Name
-                curDirectory.icon = My.Resources.folder
-                element.subItems.Add(curDirectory)
-            Next
-
-            myFiles = mydir.GetFiles
-
-            For intloop = 0 To myFiles.GetUpperBound(0)
-                If Not (myFiles(intloop).Name.ToLower = "desktop.ini" Or myFiles(intloop).Name.ToLower = "thumbs.db") Then
-
-                    curFile = New FileStartItem()
-                    curFile.Name = myFiles(intloop).Name
-
-                    myLinkInfo.Executable = myFiles(intloop).FullName
-                    ' myLinkInfo.icon = getIcon(myLinkInfo.Executable)
-                    curFile.Link = myLinkInfo
-
-                    element.subItems.Add(curFile)
-                End If
-            Next
-        End If
+        Next
         element.subItems.Sort()
         Return element
     End Function
@@ -206,16 +193,17 @@ Public Module programList
         End If
     End Sub
 
-    'Private Function getIcon(ByVal path As String) As Bitmap
-    '    Dim myIcon As Bitmap
-    '    'Try
-    '    '    myIcon = Drawing.Icon.ExtractAssociatedIcon(path).ToBitmap 'Icon.FromHandle(hIcon).ToBitmap
-    '    'Catch ex As Exception
-    '    '    myIcon = New Bitmap(1, 1)
-    '    'End Try
+    Private Function getIcon(ByVal path As String) As Bitmap
+        Dim myIcon As Bitmap
+        Try
+            myIcon = Drawing.Icon.ExtractAssociatedIcon(path).ToBitmap 'Icon.FromHandle(hIcon).ToBitmap
+        Catch ex As Exception
+            myIcon = My.Resources.folder
+            Debug.Print("Failed")
+        End Try
 
-    '    Return myIcon
-    'End Function
+        Return myIcon
+    End Function
 
 
 End Module
