4561f4e42bf71e0c58502293c4d26dd63774adf8
Surak256 <surak1024@gmail.com>
2017-07-23 00:44:32 -0400

    Some documentation to make sure I don't forget exactly what these functions do.

--- LCARSexplorer/LCARSexplorer/modCommon.vb
+++ LCARSexplorer/LCARSexplorer/modCommon.vb
@@ -3,6 +3,15 @@
 Imports System.IO
 
 Module modCommon
+    ''' <summary>
+    ''' Formats a number of bytes with appropriate units
+    ''' </summary>
+    ''' <param name="bytes">Number of bytes to format</param>
+    ''' <returns>String representation</returns>
+    ''' <remarks>
+    ''' Uses BINARY prefixes, not SI prefixes. This means each level is based on 
+    ''' 1024, not 1000.
+    ''' </remarks>
     Public Function ToDriveSize(ByVal bytes As Long) As String
         If bytes < 0 Then Return "-" & ToDriveSize(-1 * bytes)
         If bytes = 0 Then Return "0B"
@@ -16,25 +25,38 @@ Module modCommon
         Return adjustedSize.ToString("N2") & units(order)
     End Function
 
+    ''' <summary>
+    ''' Recursively find the size of a directory and all contained files
+    ''' </summary>
+    ''' <param name="d">Directory to search</param>
+    ''' <returns>Size of all files contained within the directory and its subdirectories</returns>
+    ''' <remarks>
+    ''' Inaccessible files or directories will report a size of zero.
+    ''' </remarks>
     Public Function DirSize(ByVal d As DirectoryInfo) As Long
         Dim Size As Long = 0
-        ' Add file sizes.
         Try
-            Dim fis As FileInfo() = d.GetFiles()
-            For Each fi As FileInfo In fis
-                Size += fi.Length
-            Next fi
             ' Add subdirectory sizes.
             Dim dis As DirectoryInfo() = d.GetDirectories()
             For Each di As DirectoryInfo In dis
                 Size += DirSize(di)
             Next di
+            ' Add file sizes.
+            Dim fis As FileInfo() = d.GetFiles()
+            For Each fi As FileInfo In fis
+                Size += fi.Length
+            Next fi
         Catch ex As IOException
             Return 0
         End Try
         Return Size
     End Function
 
+    ''' <summary>
+    ''' Generate a color for use with LCARS buttons from a file extension
+    ''' </summary>
+    ''' <param name="ext">File extension</param>
+    ''' <returns>Hexadecimal notation for color</returns>
     Public Function getExtColor(ByVal ext As String) As String
         ext = ext.Replace(".", "").ToLower
 
@@ -51,6 +73,12 @@ Module modCommon
         Return System.Drawing.ColorTranslator.ToHtml(Color.FromArgb(255, c(0), c(1), c(2)))
     End Function
 
+    ''' <summary>
+    ''' Check if a FileAttributes variable contains the specified flag or flags
+    ''' </summary>
+    ''' <param name="fileAttributes">Attributes to check</param>
+    ''' <param name="flag">Flag(s) to find</param>
+    ''' <returns>True if flag(s) is/are present</returns>
     Public Function FileHasFlag(ByVal fileAttributes As FileAttributes, ByVal flag As FileAttributes) As Boolean
         Return (fileAttributes And flag) = flag
     End Function
